e9baa7d8095cfc6b1be8b396dee11dee0da1131e
Мартини Персей ТКИ-141 <pmartini@yandex.ru>
2025-09-23 14:01:14 +0300

    Create 2.1.c

---
+++ 2.1.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+/**
+ * @brief Рассчитывает функцию sum (сумма x и y) по заданным параметрам
+ * @param x значение параметра x
+ * @param y значение параметра x
+ * @return возвращает функцию назад
+ */
+double sum(double x, double y);
+
+/**
+ * @brief Рассчитывает функцию dif (разность x и y) по заданным параметрам
+ * @param x значение параметра x
+ * @param y значение параметра x
+ * @return возвращает функцию назад
+ */
+
+double dif(double x, double y);
+
+/**
+ * @brief Рассчитывает функцию mult (произведение x и y) по заданным параметрам
+ * @param x значение параметра x
+ * @param y значение параметра x
+ * @return возвращает функцию назад
+ */
+
+double mult(double x, double y);
+
+/**
+ * @brief Рассчитывает функцию sep (частное x и y) по заданным параметрам
+ * @param x значение параметра x
+ * @param y значение параметра x
+ * @return возвращает функцию назад
+ */
+
+double sep(double x, double y);
+
+/**
+ * @brief точка входа в программу
+ * @return возвращает 0, если программа выполнена корректно; 1 - если некорректно
+ */
+
+int main(void) {
+    int a;
+    double x;
+    double y;
+    printf("Insert x and y (format:x y)\n");
+    scanf("%lf %lf", &x, &y);
+    printf("insert function number which you want to do (1-4):\n1 - sum\n2 - difference\n3 - multiplication\n4 - separation\n");
+    scanf("%d", &a);
+
+    while (a < 1 || a > 4) {
+        printf("Incorrect choice. Try again. insert function number which you want to do (1-4):\n1 - sum\n2 - difference\n3 - multiplication\n4 - separation\n");
+        scanf("%d", &a);
+    }
+    if (a == 1) {
+        printf("Using sum. Result = %lf\n", sum(x, y));
+    }
+    else if (a == 2) {
+        printf("Using dif. Result = %lf\n", dif(x, y));
+    }
+    else if (a == 3) {
+        printf("Using mult. Result = %lf\n", mult(x, y));
+    }
+    else if (a == 4) {
+        printf("Using sep. Result = %lf\n", sep(x, y));
+    }
+    return 0;
+}
+
+double sum(double x, double y) {
+    return x + y;
+}
+
+double dif(double x, double y) {
+    return x - y;
+}
+
+double mult(double x, double y) {
+    return x * y;
+}
+
+double sep(double x, double y) {
+    return x / y;
+}
